bdb93c6bee38e71607591770efa4c70a18d11066
Molnar Kristian <mkrisztian95@gmail.com>
2017-04-05 11:27:49 +0300

    first commit

---
+++ GifCreator/PostCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.swift
+//  GifCreator
+//
+//  Created by Molnar Kristian on 4/3/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+}
